b7cecdacefec2207c32d62227636160957c8a231
Athly23 <andrewly06@gmail.com>
2020-12-18 15:14:26 -0500

    division components and division json added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import Addition from './Components/Addition'
-import SubtractionData from './data/subtraction.json'
-import AdditionData from './data/additon.json'
+import Addition from "./Components/Addition";
+import Division from "./Components/Division";
+import SubtractionData from "./data/subtraction.json";
+import AdditionData from "./data/additon.json";
+import DivisionData from "./data/division.json";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   let subtractionList = SubtractionData.map((dataset, i) => {
-    return <div key={i}>
-      <p>{dataset.problem}</p>
-      <p>{dataset.answer}</p>
-    </div>
-  })
+    return (
+      <div key={i}>
+        <p>{dataset.problem}</p>
+        <p>{dataset.answer}</p>
+      </div>
+    );
+  });
   let additionList = AdditionData.map((dataset, i) => {
-    return <div key={i}>
-      <p>{dataset.problem}</p>
-      <p>{dataset.answer}</p>
-    </div>
-  })
+    return (
+      <div key={i}>
+        <p>{dataset.problem}</p>
+        <p>{dataset.answer}</p>
+      </div>
+    );
+  });
+  let divisionList = DivisionData.map((dataset, i) => {
+    return (
+      <div key={i}>
+        <p>{dataset.problem}</p>
+        <p>{dataset.answer}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       {/* {subtractionList}
       {additionList} */}
       <Addition />
-
+      <Division />
     </div>
   );
 }
