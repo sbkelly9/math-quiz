c604ca374ecaee2e3ac45b1e5571b0bc16d99d63
Stu Kelly <sbkelly9@gmail.com>
2021-01-07 12:14:09 -0500

    fixing paths

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import Navbar from "./Navbar";
 import Addition from "./Components/Addition";
 import Subtraction from "./Components/Subtraction";
 import SubtractionData from "./data/subtraction.json";
@@ -5,6 +8,7 @@ import AdditionData from "./data/additon.json";
 import Division from "./Components/Division";
 import DivisionData from "./data/division.json";
 import "./App.css";
+
 
 function App() {
   let subtractionList = SubtractionData.map((dataset, i) => {
@@ -15,32 +19,53 @@ function App() {
       </div>
     );
   });
-  let additionList = AdditionData.map((dataset, i) => {
-    return (
-      <div key={i}>
-        <p>{dataset.problem}</p>
-        <p>{dataset.answer}</p>
-      </div>
-    );
-  });
-  let divisionList = DivisionData.map((dataset, i) => {
-    return (
-      <div key={i}>
-        <p>{dataset.problem}</p>
-        <p>{dataset.answer}</p>
-      </div>
-    );
-  });
+  // let additionList = AdditionData.map((dataset, i) => {
+  //   return (
+  //     <div key={i}>
+  //       <p>{dataset.problem}</p>
+  //       <p>{dataset.answer}</p>
+  //     </div>
+  //   );
+  // });
+  // let divisionList = DivisionData.map((dataset, i) => {
+  //   return (
+  //     <div key={i}>
+  //       <p>{dataset.problem}</p>
+  //       <p>{dataset.answer}</p>
+  //     </div>
+    // );
+  // });
+
+
 
   return (
     <div className="App">
+      <nav>
+        <Link to="/">
       {subtractionList}
-      {additionList}
-      {divisionList}
-      <Addition />
-      <Division />
+      <h1>Subtraction Test</h1>
+      </Link>
+      <Link to="/addition"/>
+    
+      </nav>
+      <main>
+        {/* <Route path="/addition"
+          component={Addition}
+        /> */}
+        <Route path="/" exact component={Home} />
+          <Route path="/addition/" component={Addition} />
+          <Route path="/subtraction" exact component={Subtraction} />
+          {/* <Route path="/multiplication" exact component={Multiplication} /> */}
+          <Route path="/division" exact component={Division} />
+      </main>
+      {/* {additionList}
+      {divisionList} */}
+      {/* <Addition />
+      <Division /> */}
     </div>
   );
 }
+
+
 
 export default App;
